4239b6d3a8746c25d3567abfc2a4022c7779791d
sarastackhouse <sarapalacios7@gmail.com>
2021-02-20 21:37:28 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import LogIn from './components/auth/LogIn';
+import SignUp from './components/auth/SignUp';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={LogIn}/>
+      <Route exact path="/signup" component={SignUp}/>
+    </Switch>
+  </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import NavBar from './layout/NavBar';
+import Footer from '../components/layout/Footer'
+
+
+const Home = () => {
+    return ( 
+        <Fragment>
+            <NavBar/>
+                <h1>Soy la home</h1>
+            <Footer/>
+        </Fragment>
+        
+     );
+}
+ 
+export default Home;
